9d66bd11aa611ea38b635d88e6b9909707dbc9ff
camilotoruno <131733441+camilotoruno@users.noreply.github.com>
2024-02-26 13:30:39 -0500

    Remove plotting functions, add verbose arg
    Remove plotting functions from initial development work that showed histograms for the buildstock data. Also add verbose argument to reduce the output's clutter.

--- demo_env/buildstock_filtering.py
+++ demo_env/buildstock_filtering.py
@@ -10,109 +10,50 @@ city, and city size.
 """
 
 import pandas as pd 
-import matplotlib.pyplot as plt
 import numpy as np 
 import random
 from argparse import ArgumentParser
 import os 
 
-# def plotting_1():
-#     total_income_bins = create_income_bins(obuildstock)
-    
-#     # check the income distribution differences between the subsample and original 
-#     obuildstock2 = obuildstock
-#     subset_income_bins = create_income_bins(obuildstock2)
-#     diff = subset_income_bins.percentages - total_income_bins.percentages
-#     # plt.bar(subset_income_bins.index, diff)
-#     # plt.title('Percentage Income Shifts')
-#     # plt.xticks(rotation=90)  # Rotate labels 90 degrees counterclockwise
-#     # plt.show()
-    
-#     city_bins = obuildstock.value_counts('in.city')
-    
-#     # Create the histogram
-#     plt.bar(subset_income_bins.index, subset_income_bins.values)
-#     plt.xlabel("Low End of Annual Income")  # Replace with the label for the x-axis
-#     plt.xticks(subset_income_bins.index, rotation=90)  # Rotate labels 90 degrees counterclockwise
-#     plt.ylabel("Number of Houses Matching Criteria")
-#     plt.title("Histogram of Houses Matching Criterion")
-#     plt.show()
 
-
-# def plotting_2(subset_income_bins):
-#     # calculate the numebr of houses up to and including the income cutoff
-#     per = []
-#     no_houses = []
-#     for income in subset_income_bins.index:
-#         no_houses.append(sum(subset_income_bins[:income]))
-#         per.append(sum(subset_income_bins[:income])/sum(subset_income_bins)*100)
+# def det_num_small_cities_can_sim(city_bins):
+#     sum_tot = 0
+#     city_bins = city_bins.sort_values()
+#     city_count = 2
+#     for i in range(0, len(city_bins)):
+#         if sum_tot + city_bins[i] < city_count*400:
+#             sum_tot = sum_tot + city_bins[i]
+#         else: break
     
-#     fig, graph1 = plt.subplots()
+#     for j in range(0, i):
+#         print(city_bins.index[j])
+#     print(i, 'cities for price of ', city_count)
     
-#     graph1.bar(subset_income_bins.index, no_houses)
-#     plt.xticks(subset_income_bins.index, rotation=90)  # Rotate labels 90 degrees counterclockwise
-#     # plt.yticks(np.linspace(min(no_houses),max(no_houses), len(subset_income_bins)))
-#     plt.yticks(no_houses)
-    
-#     graph2 = graph1.twinx()
-#     graph2.bar(subset_income_bins.index, per)
-#     plt.xlabel("Low End of Annual Income")  # Replace with the label for the x-axis
-#     plt.ylabel("Per. Houses Up to and Including an Income Level")
-#     plt.xticks(subset_income_bins.index, rotation=90)  # Rotate labels 90 degrees counterclockwise
-#     # plt.yticks(np.linspace(min(per),max(per), len(subset_income_bins)))
-#     plt.yticks(per)
-#     plt.title("Houses with Income Equal & Above a Bin Value")
-    
-#     plt.grid()
-#     plt.show()
-    
-#     # plt.bar(range(0, len(city_bins)), city_bins.values)
-#     # plt.title('cities of 1-400 houses')
-#     # plt.xticks(rotation=90)  # Rotate labels 90 degrees counterclockwise
-#     # plt.show()
-#     # print('sum of small cities, ', sum(city_bins.values))
-
-
-def det_num_small_cities_can_sim(city_bins):
-    sum_tot = 0
-    city_bins = city_bins.sort_values()
-    city_count = 2
-    for i in range(0, len(city_bins)):
-        if sum_tot + city_bins[i] < city_count*400:
-            sum_tot = sum_tot + city_bins[i]
-        else: break
-    
-    for j in range(0, i):
-        print(city_bins.index[j])
-    print(i, 'cities for price of ', city_count)
-    
-    print('\n\nRemember to re-ID the BUILDING ID!')
+#     print('\n\nRemember to re-ID the BUILDING ID!')
     
 
-def filter_cities(buildstock, keep_cities, exclude_cities):
+def filter_cities(buildstock, keep_cities, exclude_cities, verbose):
     # note this modifies the original buildstock object (?)
     
-    print('\nfiltering cities')
-
     if exclude_cities != []:
         rows =  ~buildstock['in.city'].isin(exclude_cities)
         buildstock = buildstock[rows ]
-        print('\tremoving ', sum(~rows), ' exclude cities houses')
+        if verbose: print('\tremoving ', sum(~rows), 'for exclude cities arg')
         
     if keep_cities != []:
         rows = buildstock['in.city'].isin(keep_cities)
-        print('\reamining ', sum(~rows), ' kept houses in cities houses')
         buildstock = buildstock[rows]   
+        if verbose: print('\tremoving ', sum(~rows), 'for keep cities arg')
 
     return buildstock
 
 
-def filter_city_size(buildstock, city_size_limit, keep_cities):
+def filter_city_size(buildstock, city_size_limit, keep_cities, verbose):
     # for each city, limit the number of houses randomly 
     
     def count_remaining_houses(keep_cities):
         for city in keep_cities:
-            print('\t', len( buildstock[ buildstock['in.city'] == city ] ), 'houses in', city)
+            print('\tWorking with', len( buildstock[ buildstock['in.city'] == city ] ), 'houses in', city)
     
     
     def generate_unique_list(city_size_limit, num_houses_in_city):
@@ -145,8 +86,9 @@ def filter_city_size(buildstock, city_size_limit, keep_cities):
         return unique_list
         
     
-    print("Filtering by city size:", city_size_limit)
-    count_remaining_houses(keep_cities)
+    if verbose: 
+        print("Filtering by city size:", city_size_limit)
+        count_remaining_houses(keep_cities)
     
     try: 
         # create a new table with the desired number of houses per city 
@@ -175,29 +117,31 @@ def create_income_bins(buildstock):
     return income_bins
 
 
-def filter_poverty(buildstock, acceptable_federal_poverty_levels):
-    print('filtering by poverty levels')
+def filter_poverty(buildstock, acceptable_federal_poverty_levels, verbose):
+    if verbose: print('\tFiltering by poverty levels...')
     buildstock = buildstock[ buildstock['in.federal_poverty_level'].isin(acceptable_federal_poverty_levels) ]
-    print('\t', len(buildstock), 'house records remaining')
+    if verbose: print('\t', len(buildstock), 'house records remaining')
     return buildstock
 
 def check_unique(buildstock):
     print('unique ratio', len(np.unique(buildstock.bldg_id.values))/len(buildstock))
-    
-
 
 
 def filtering(buildstock_folder, buildstock_file, federal_poverty_levels, 
               city_size_limit, keep_cities, exclude_cities, output_file, output_folder,
-              save = False):   
-    print('Loading data..')
+              save = False, verbose = False):   
+    
+    if not os.path.exists(os.path.join(buildstock_folder, buildstock_file)):
+        raise OSError(f'Could not find buildstock file {os.path.join(buildstock_folder, buildstock_file)}')
+    
+    print('Loading buildstock and filtering..')
     ibuildstock = pd.read_csv(os.path.join(buildstock_folder + buildstock_file))
-    obuildstock = filter_cities(ibuildstock, keep_cities, exclude_cities)
-    obuildstock = filter_poverty(obuildstock, federal_poverty_levels)
+    obuildstock = filter_cities(ibuildstock, keep_cities, exclude_cities, verbose)
+    obuildstock = filter_poverty(obuildstock, federal_poverty_levels, verbose)
     obuildstock = obuildstock.reset_index(drop=True)   
-    check_unique(obuildstock)
-    obuildstock = filter_city_size(obuildstock, city_size_limit, keep_cities)
-    check_unique(obuildstock)
+    # check_unique(obuildstock)
+    obuildstock = filter_city_size(obuildstock, city_size_limit, keep_cities, verbose)
+    # check_unique(obuildstock)
     
     # reset the bldg index ids to ascending (maybe needed if metadata ran thru the ResStock/Openstudio workflows)
     # obuildstock['bldg_id'] = obuildstock.index.values + 1
@@ -211,21 +155,4 @@ def filtering(buildstock_folder, buildstock_file, federal_poverty_levels,
 
 if __name__ == "__main__":
     filtering()
-
-#%% clean and filter buildstock
-
-
-
-#%% Analyze the remaining housing stock 
-
-# plotting_1()
-
-#%%
-
-# plotting_2()
-
-
-#%% determine the number of small cities we could simulate for the price of a large city
-# det_num_small_cities_can_sim(city_bins)
-
 
