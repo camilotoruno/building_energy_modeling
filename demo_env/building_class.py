44b1f3d07b419556ed5635407a43dc6d299fa534
camilotoruno <131733441+camilotoruno@users.noreply.github.com>
2024-02-27 03:55:01 -0500

    Create building_class as separate file
    Move building class definition from xml modifier script. Add assign_folders_contents function to update the bldg object with files from the folder it is in.

---
+++ demo_env/building_class.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 20:32:22 2024
+
+@author: camilotoruno
+"""
+import os 
+
+class BuildingFilesData:
+    """
+    Represents a custom object with folder, schedule, and xml attributes.
+    """
+    def __init__(self, ID):
+        # require initialization with the folder of the files for the building
+        self.id = ID
+        self.folder = None
+        self.zip = None
+        
+        self.xml = None        
+        self.modified_xml = None        
+
+        self.idf = None
+        self.oedi_zip_fldr = None
+        self.city = None    
+        
+        self.schedules = None
+        self.schedules_new = None
+    
+    def assign_folders_contents(self):  
+        for file in os.listdir(self.folder):
+            filepath = os.path.join(self.folder, file)
+            if "schedules.csv" in file and self.schedules == None:
+                # original schedules file downloaded for OEDI 
+                self.schedules = filepath
+            elif "in.xml" in file and self.xml == None:
+                self.xml = filepath
+            elif "in.idf" in file and self.idf == None:
+                self.idf = filepath 
+            elif ('schedules' in file) and ("schedules.csv" not in file) and (self.schedules_new == None):
+                # case for the genereated schedules file with a large
+                # alpha numeric suffix that openstudio generates and 
+                # points the .idf files to.  
+                self.schedules_new = filepath
+                
+
+if __name__ == "__main__":
+    # Code that executes only when the script is run directly
+    # (This part is optional and can be left empty)
+    None
